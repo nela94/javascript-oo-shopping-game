944a517eac6f111279cf9e80ff8355eb819d10c9
Manuela Sanchez <m.sanchez947@Hotmail.com>
2023-03-14 17:46:29 -0400

    completed module 3

--- js/shoppinggame.js
+++ js/shoppinggame.js
@@ -210,10 +210,20 @@ function loadMasterData() {
 }
 
 // Complete this function
-const findProductById = (id) => {};
+const findProductById = (id) => {
+    return function(product){
+        if(id == product.id){
+            return true;
+        }else{
+            return false;
+        }
+    };
+};
 
 // Complete this function
-const generateProductId = () => {};
+const generateProductId = () => {
+    return Math.floor(Math.random() * 20) + 1;
+};
 
 
 const getProduct = (prodList, pId) => {
